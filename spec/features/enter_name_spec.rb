43d1841c087b405cf119fa2cda769cd03c2c5759
jamierose <jrose111284@gmail.com>
2016-02-04 13:44:04 +0000

    added web helper

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
+require 'web_helpers'
+
 feature 'Enter names' do
   scenario 'submitting names' do
     visit '/'
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
+require 'web_helpers'
+
 feature 'view hit points' do
   scenario "see player 2 hit points" do
     visit '/'
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
   end
 end
